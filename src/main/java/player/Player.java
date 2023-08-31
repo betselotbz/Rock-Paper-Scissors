ab8b617486330038f250674bb8720b10eb75f31b
Betselot Bezabeh <betselotbz@gmail.com>
2023-08-31 16:26:00 -0500

    added HumanPlayer subclass

--- src/main/java/player/Player.java
+++ src/main/java/player/Player.java
@@ -2,41 +2,51 @@ package player;
 
 public abstract class Player {
 
-        private int points;
-        private String playerName;
-        public boolean play;
+    private int points;
+    private String playerName;
+    public boolean play;
 
-    public Player(int points, String playerName, boolean play){
+    public Player(int points, String playerName, boolean play) {
         this.playerName = playerName;
         this.points = points;
         this.play = play;
     }
+
     //Setter is a way to change value of a private variable inside a class
     //Getter allows to access the value of a private varable outside the class
     public int getPoints() {
         return this.points;
     }
 
+
     //setting new value to the as a parameter to the method
     public void setPoints(int points) {
-        this.points= points;
+        this.points = points;
     }
+
     public String getPlayerName() {
         return this.playerName;
     }
-    public void setPlayerName(String playerName){
+
+    public void setPlayerName(String playerName) {
         this.playerName = playerName;
     }
+
     public boolean getPlay() {
         return this.play;
     }
+
     public void setPlay(boolean play) {
         this.play = play;
     }
 
 
-
-
-
-
 }
+
+
+
+
+
+
+
+
