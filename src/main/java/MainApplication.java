28480583b3efb82b7c2612af81e0da700fb91159
Betselot Bezabeh <betselotbz@gmail.com>
2023-09-02 16:33:37 -0500

    added a playAgainstAnotherPlayer method

--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class MainApplication {
     public static void main(String[] args) {
+
         /*Creates an instance of the Scanner class called scanner input from the user through the standard input stream (typically the keyboard*/
         Scanner scanner = new Scanner(System.in);
         //To store user's choice we declare with a variable type String
@@ -12,13 +13,16 @@ public class MainApplication {
         System.out.println("1. Play against another player");
         System.out.println("2. Play against computer");
         System.out.println("E. End");
-        System.out.println("Plese, enter your choice here: ");
+        System.out.println("Please, enter your choice here: ");
+
 
         //make sure to read user's choice and convert it to lowercase
          choice = scanner.nextLine().toLowerCase();
 
          if (choice.equals("1")) {
              System.out.println("You have chosen to play against another player");
+             //calling function to display "Player 1. Rock, Paper or Scissors"
+             playAgainstAnotherPlayer();
          } else if (choice.equals("2")) {
              System.out.println("You have chosen to play against computer");
          } else if (choice.equals("E")) {
@@ -26,8 +30,18 @@ public class MainApplication {
          } else {
              System.out.println("Invalid. Please enter '1', '2' or 'E'");
          }
-
          scanner.close();
+    }
+    public static void playAgainstAnotherPlayer() {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Player 1. Rock, Paper or Scissors");
+        String player1 = scanner.nextLine();
+
+        System.out.println("Player 2. Rock, Paper or Scissors");
+        String player2 = scanner.nextLine();
+        
+
+        scanner.close();
     }
 }
