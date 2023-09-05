1de5373900a807fea044f74dc5254f5dd771e0aa
Betselot Bezabeh <betselotbz@gmail.com>
2023-09-04 23:01:27 -0500

    fixed a bug by removing closed scanner in main and passing scanner argument to playAgainstPlayer method

--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -3,7 +3,7 @@ import player.Player;
 import java.util.Scanner;
 import java.util.Random;
 import player.ComputerPlayer;
-import player.GameLogic;
+
 
 public class MainApplication {
     public static void main(String[] args) {
@@ -32,7 +32,7 @@ public class MainApplication {
         while(continuePlaying) {
             if (choice.equals("1")) {
                 System.out.println("You have chosen to play against another player");
-                playAgainstAnotherPlayer(); //calling method
+                playAgainstAnotherPlayer(scanner); //calling method
             } else if (choice.equals("2")) {
                 System.out.println("You have chosen to play against computer");
                 //storing random move in computerAction variable
@@ -53,8 +53,8 @@ public class MainApplication {
     }
     /*Method to play against another player
     Params1: scanner - scanner will read user input*/
-    public static void playAgainstAnotherPlayer() {
-        Scanner scanner = new Scanner(System.in);
+    public static void playAgainstAnotherPlayer(Scanner scanner) {
+        Scanner choice = new Scanner(System.in);
 
         System.out.println("Player 1. Rock, Paper or Scissors");
         String player1Action = scanner.nextLine().toLowerCase();
@@ -65,7 +65,6 @@ public class MainApplication {
         String result = findWinner(player1Action, player2Action);
         System.out.println(result);
 
-        scanner.close();
     }
     /*Method to play against computer
     Params1: scanner - scanner will read user input*/
@@ -112,5 +111,49 @@ public class MainApplication {
     private static boolean checkValidInput(String input) {
         return input.equals("rock") || input.equals("paper") || input.equals("scissors");
     }
+    /*Method to find the Winner between Player1 vs Player 2
+     Params1: player1Action - Player 1 action to input either rock, paper, or scissors
+     Params2: player2Action - Player 2 action to input either rock, paper, or scissors */
+    public static String findWinner(String player1Action, String player2Action) {//defined a method takes players action as input
 
+        if (player1Action.equals("rock") && player2Action.equals("paper")) {
+            return ("Player 2 Wins!");
+        } else if (player1Action.equals("paper") && player2Action.equals("rock")) {
+            return ("Player 1 Wins!");
+        } else if (player1Action.equals("scissors") && player2Action.equals("rock")) {
+            return ("Player 2 Wins!");
+        } else if (player1Action.equals("rock") && player2Action.equals("scissors")) {
+            return ("Player 1 Wins!");
+        } else if (player1Action.equals("paper") && player2Action.equals("scissors")) {
+            return ("Player 2 Wins!");
+        } else if (player1Action.equals("scissors") && player2Action.equals("paper")) {
+            return ("Player 1 Wins");
+        } else {
+            return ("Tie!");
+        }
+
+    }
+    /*Method to find the Winner between Player1 vs Computer
+    Params1: player1Action - Player 1 action to input either rock, paper, or scissors
+    Params2: computerAction - Computer action to input either rock, paper, or scissors */
+    public static String findWinner2 (String player1Action, String computerAction) {//defined a method takes players action as input
+
+        if (player1Action.equals("rock") && computerAction.equals("paper")) {
+            return ("Computer Wins!");
+        } else if (player1Action.equals("paper") && computerAction.equals("rock")) {
+            return ("Player 1 Wins!");
+        } else if (player1Action.equals("scissors") && computerAction.equals("rock")) {
+            return ("Computer Wins!");
+        } else if (player1Action.equals("rock") && computerAction.equals("scissors")) {
+            return ("Player 1 Wins!");
+        } else if (player1Action.equals("paper") && computerAction.equals("scissors")) {
+            return ("Computer Wins!");
+        } else if (player1Action.equals("scissors") && computerAction.equals("paper")) {
+            return ("Player 1 Wins");
+        } else {
+            return ("Tie!");
+        }
+    }
 }
+
+
